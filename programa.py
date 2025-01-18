7e569a9199c254e0aecc935456286165faa4eb40
jarytwist <jaroldgeordanno@gmail.com>
2025-01-17 21:10:56 -0600

    Añadir función menu

---
+++ programa.py
@@ -0,0 +1,12 @@
+#Impresiones de pantalla
+def menu():
+    print("Seleccione una opción:")
+    print("1. Suma de n números")
+    print("2. Producto entre n números")
+    print("3. División entre 2 números")
+    print("4. Calcular el factorial de n")
+    print("5. Tablas de multiplicar")
+    print("6. Cuadrado y cubo de un número")
+    print("7. Promedio de una serie de números")
+    print("8. Valores máximo y mínimo")
+    print("9. Salir")
